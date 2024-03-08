69dd1c4d0e3dad8e3690f98ed4566b1e16b87dbe
Daniel Wieczorek <121404035+byte4RR4Y@users.noreply.github.com>
2024-03-08 09:36:51 +0100

    Add files via upload

--- config/installkernel.sh
+++ config/installkernel.sh
@@ -5,4 +5,5 @@ ARCHIVE=$1
 BUILD="$(unzip -l ${ARCHIVE} boot/vmlinuz-* | sed -n 's|^.*boot/vmlinuz-\(.*\)$|\1|p')"
 unzip -o "${ARCHIVE}" -d /
 update-initramfs -c -v -k "${BUILD}"
+ln -s /usr/src/linux-headers-$(uname -r)/include/linux /usr/include/linux
 
